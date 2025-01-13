901e736e3716d9c518f81fa898951447197c02bb
Sasha Murokh <smurokh50@stuy.edu>
2025-01-13 11:35:54 -0500

    minor edits for embeds

--- app/spotify.py
+++ app/spotify.py
@@ -99,6 +99,6 @@ def album_info(token_header, id):
     output['tracks'] = tracks
     return(output) #returns dictionary
 
-
+#from spotifys website
 def get_embed_html(token_header, id):
     return f'<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/{id}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>'
